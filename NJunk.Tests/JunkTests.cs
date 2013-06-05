b2838e781a6b5dcd43a4c8bc9997506c28204cf2
Jeff Sharp <jeff.sharp@git>
2013-06-04 19:55:22 -0500

    Create project structure

---
+++ NJunk.Tests/JunkTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NJunk.Tests
+{
+    public class JunkTests
+    {
+    }
+}
---
+++ NJunk/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Security;
+
+// Description
+[assembly: AssemblyTitle       ("NJunk")]
+[assembly: AssemblyDescription (".NET Junk Data Generator")]
+
+// Security
+//
+// All code is transparent; the entire assembly will not do anything privileged or unsafe.
+// http://msdn.microsoft.com/en-us/library/dd233102.aspx
+//
+[assembly: SecurityTransparent]
